87ebc8cc54fad49aa5d518c9781f2d64dd9e4dfd
Harrison Shoebridge <harrison@theshoebridges.com>
2016-09-05 21:43:36 +1000

    Add GitHub integration

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"os"
+	"net/http"
 	"os/exec"
-	"path/filepath"
 )
 
 var (
@@ -14,17 +15,25 @@ var (
 	remote = flag.String("remote", "origin", "The remote of the git repository which you want to push to")
 	branch = flag.String("branch", "master", "The branch of the remote git repository which you want to push to")
 
-	forkOwner = flag.String("fork-owner", "", "The repository which will be used to send a Pull Request GitHub")
-	repo      = flag.String("repo", "", "The repository name of the repository on GitHub")
+	forkOwner = flag.String("fork-owner", "", "The owner of the repository the Pull Request will be sent from")
+	forkRepo  = flag.String("fork-repo", "", "The repository the Pull Request will be sent from of")
+
+	owner    = flag.String("owner", "", "The owner of the repository which the Pull Request will be sent to")
+	repo     = flag.String("repo", "", "The repository which the Pull Request will be sent to")
+	toBranch = flag.String("to-branch", "master", "The branch which you would like your changes merged into")
+
+	token = flag.String("token", "", "The API token which has access to both the fork and main repositories")
 
 	updateSubmodules = exec.Command("git", "submodule", "update", "--remote", "--merge")
 	checkDiff        = exec.Command("git", "status", "--porcelain")
 	addChanges       = exec.Command("git", "add", "-A")
 	commitChanges    = exec.Command("git", "commit", "-m Update submodules")
-	pushChanges      *exec.Cmd // pushChanges is set later as it relies on the remote and branch flags. takes the form: `git push <remote> <branch>`
+	pushChanges      *exec.Cmd // pushChanges is set later as it relies on the remote and branch flags. In the form: `git push <remote> <branch>`
 )
 
 func main() {
+	var err error
+
 	flag.Parse()
 
 	pushChanges = exec.Command("git", "push", *remote, *branch)
@@ -93,14 +102,31 @@ func main() {
 
 	pr := pullRequest{
 		Title: "Update submodules",
-		Body:  "Update submodules",
-		Head:  "",
-		Base:  "",
+		Body:  "Hey! I'm just your friendly neighbourhood bot, updating your submodules.",
+		Head:  fmt.Sprintf("%v:%v", *forkOwner, *branch),
+		Base:  *toBranch,
 	}
+
+	fmt.Println(pr)
 
 	var buf bytes.Buffer
 
-	info := json.NewEncoder(&buf).Encode()
+	err = json.NewEncoder(&buf).Encode(pr)
+	if err != nil {
+		fmt.Println("Couldn't encode JSON:", err)
+		return
+	}
+
+	endpoint := fmt.Sprintf("https://api.github.com/repos/%v/%v/pulls?access_token=%v", *owner, *repo, *token)
+	resp, err := http.Post(endpoint, "application/json", &buf)
+	if err != nil {
+		fmt.Println("Couldn't create PR :", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Println("I've been waiting a while to say this: My work here... is done!")
 }
 
 type pullRequest struct {
